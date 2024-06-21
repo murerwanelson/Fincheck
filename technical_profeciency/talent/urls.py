d6b26b3b2695078796b2eed34b7f2c96f84dbd8a
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2024-06-21 21:29:32 +0200

    Update urls.py

--- technical_profeciency/talent/urls.py
+++ technical_profeciency/talent/urls.py
@@ -1,7 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 from . import views
 from django.urls import path
 
+
+router = DefaultRouter()
+router.register(r'employees', views.EmployeeViewSet)
+router.register(r'companies', views.CompanyViewSet)
+
 urlpatterns = [
     path('bulk-upload/', views.bulk_upload, name='bulk_upload'),
+    path('api/', include(router.urls)),
     # Add other URL patterns here
 ]
+
