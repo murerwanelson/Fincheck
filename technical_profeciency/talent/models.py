35c79fb83166e61bba2c83ecb7a85f9065b4a7ab
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2024-06-21 15:53:44 +0200

    Update models.py

--- technical_profeciency/talent/models.py
+++ technical_profeciency/talent/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from fernet_fields import EncryptedCharField
 
 # Create your models here.
+
 class Company(models.Model):
     name = models.CharField(max_length=255)
     registration_date = models.DateField()
@@ -15,7 +15,6 @@ class Employee(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     employee_id = models.CharField(max_length=50, blank=True, null=True)
-    employee_id = EncryptedCharField(max_length=50, blank=True, null=True)
     department = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
     start_date = models.DateField()
