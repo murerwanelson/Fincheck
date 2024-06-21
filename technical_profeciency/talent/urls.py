60ec7f552e505e36bf1675a6e5df34c9c0feb5f8
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2024-06-21 11:23:17 +0200

    Update urls.py

--- technical_profeciency/talent/urls.py
+++ technical_profeciency/talent/urls.py
@@ -1,6 +1,7 @@
-from .views import main
-from django.urls import path, include
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('',main)
+    path('bulk-upload/', views.bulk_upload, name='bulk_upload'),
+    # Add other URL patterns here
 ]
