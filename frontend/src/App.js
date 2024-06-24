47cd10a14a132fd81ae8ea3f0be0d30350679bfb
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2024-06-24 10:56:30 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import EmployeeList from './components/EmployeeList';
 import EmployeeForm from './components/EmployeeForm';
+import BulkUpload from './components/BulkUpload';
 
 const App = () => {
   const [refresh, setRefresh] = useState(false);
@@ -15,6 +16,7 @@ const App = () => {
       <h1>Employee Management</h1>
       <EmployeeForm onSave={handleSave} />
       <EmployeeList key={refresh} />
+      <BulkUpload />
     </div>
   );
 };
