c2dc66591b9c20484216116bab007529b38faaef
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2024-06-25 16:32:20 +0200

    .

--- technical_profeciency/talent/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class SecurityTestCase(TestCase):
-    def test_encryption(self):
-        # Test that sensitive fields are encrypted
-        pass
-
